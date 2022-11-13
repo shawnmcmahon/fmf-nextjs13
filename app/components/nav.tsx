8b91ad92565871e41c45c5a97c4c6711012cb918
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-11-13 15:51:56 -0700

    Begin home page styling

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const nav = () => {
   return (
-    <header className="sticky top-0 z-50 text-sm font-medium text-white bg-gray-900 border-b border-gray-800/75">
+    <header className="sticky top-0 z-50 text-sm font-medium bg-gray-900 border-b border-gray-800/75">
       <div className="flex items-center justify-between h-16 max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
-        <nav className="hidden sm:flex sm:items-center sm:gap-4 lg:gap-8 text-white">
-          <a href="/routes" className="text-white">Routes</a>
+        <nav className="hidden sm:flex sm:items-center sm:gap-4 lg:gap-8 ">
+          <a href="/routes" className="">Routes</a>
           <a href="/createRoute" className="">Create Route</a>
         </nav>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,16 @@ import Nav from './components/nav'
 
 export default function Home() {
   return (
-    <div className="border-4 border-red-700 bg-[#111827] w-100 h-screen min-h-full flex flex-col">
+    <div className="border-4 border-red-700 bg-[#DFCEE6] w-100 h-screen min-h-full flex flex-col">
       <Nav />
-          <h1 className="">
-            FMF
-          </h1>
+      <section className="">
+        <div className="w-100 h-full">
+          <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
+            <p className="order-first bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text font-medium tracking-wide text-transparent text-center">Quick Sharable Maps</p>
+            <p className="text-4xl font-extrabold uppercase tracking-tighter text-transparent sm:text-5xl lg:text-7xl text-center">Find Me Fast</p>
+          </div>
+        </div>
+      </section>
     </div>
     )
 }
