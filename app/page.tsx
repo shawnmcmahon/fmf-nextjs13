7d5a90067ea43f6d6e07e0fd0227158c649291a2
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-11-21 17:38:31 -0700

    import Link from next

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Nav from './components/nav'
 import addCircle from '../assets/images/addCircle.png'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Home = () => {
   return (
@@ -13,8 +14,9 @@ const Home = () => {
             <p className="text-4xl font-extrabold uppercase tracking-tighter text-transparent sm:text-5xl lg:text-7xl text-center">Find Me Fast</p>
             <p className="mx-auto mt-8 max-w-2xl text-xl font-medium text-white text-center">Create quick, sharable routes with anyone using Google Maps.</p>
             <p className="mx-auto mt-0 max-w-2xl text-xl font-medium text-white text-center">Step-by-step maps to help you get found no matter where you are.  </p>
-            <div className="flex flex-col-reverse justify-center  place-items-center max-w-max">
-              <button className="max-w-max flex flex-col-reverse place-items-center border-2"> 
+            <div className="flex flex-col-reverse justify-center  place-items-center max-w-max m-8">
+              <Link href="/create">
+              <button className="max-w-max flex flex-col-reverse place-items-center border-2 p-2 rounded-lg"> 
               <Image 
                 className=""
                 src={addCircle}
@@ -24,6 +26,7 @@ const Home = () => {
                 />
               <p className="order-first bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text font-medium tracking-wide text-transparent text-center">Add Route</p>
               </button>
+              </Link>
             </div>
           </div>
         </div>
