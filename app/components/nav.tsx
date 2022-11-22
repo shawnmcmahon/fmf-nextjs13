a7277a189ce29e666d26fe8f3bc96ee7f6b126aa
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-11-21 18:06:47 -0700

    Add styling

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -6,7 +6,7 @@ const nav = () => {
       <div className="flex items-center justify-between h-16 max-w-screen-xl px-4 mx-auto sm:px-6 lg:px-8">
         <nav className="hidden sm:flex sm:items-center sm:gap-4 lg:gap-8 ">
           <a href="/routes" className="">Routes</a>
-          <a href="/createRoute" className="">Create Route</a>
+          <a href="/create" className="">Create Route</a>
         </nav>
       </div>
     </header>
