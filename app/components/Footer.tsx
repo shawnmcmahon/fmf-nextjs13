f1e37f09ca602bec0ca014ffcdde348ab96ac7d1
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-11-26 14:35:41 -0700

    Keep footer in main page for now while I debug hydration error

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,9 +4,7 @@ import logo from '../../assets/images/logo1.png'
 
 const Footer = () => {
   return (
-    <>
-
-    <div className="bg-white dark:bg-gray-800">
+    <footer className="bg-white dark:bg-gray-800">
           <div className="max-w-screen-xl p-4 py-6 mx-auto lg:py-16 md:p-8 lg:p-10">
               <div className="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5">
                   <div>
@@ -95,7 +93,7 @@ const Footer = () => {
               {/* <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"> */}
               <div className="text-center">
                   <a href="#" className="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
-                      <Image src={logo} className="h-6 mr-3 sm:h-9" alt="Find Me Fast Logo" width={250} height={250}  />
+                      <Image src={logo} className="h-6 max-w-max mr-3 sm:h-9" alt="Find Me Fast Logo" width={250} height={250}  />
                       Find Me Fast
                   </a>
                   <span className="block text-sm text-center text-gray-500 dark:text-gray-400">© {new Date().getFullYear()}  Find Me Fast. All Rights Reserved. Built with <a href="https://flowbite.com" className="text-purple-600 hover:underline dark:text-purple-500">Flowbite</a> and <a href="https://tailwindcss.com" className="text-purple-600 hover:underline dark:text-purple-500">Tailwind CSS</a>.
@@ -117,15 +115,14 @@ const Footer = () => {
                           </a>
                       </li>
                       <li>
-                          <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
+                          <a href="https://github.com/shawnmcmahon/fmf-nextjs13" className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400">
                               <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" /></svg>
                           </a>
                       </li>
                   </ul>
               </div>
           </div>
-      </div>
-      </>
+      </footer>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -159,7 +159,7 @@ export default function page() {
                           <li className="flex space-x-3">
                               {/* <!-- Icon --> */}
                               <svg className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
-                              <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Exceptional Preformance</span>
+                              <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Exceptional Performance</span>
                           </li>
                       </ul>
                       <p className="mb-8 font-light lg:text-xl">Deliver great directions fast -  with the world&#39;s most trusted GPS.</p>
@@ -565,7 +565,6 @@ export default function page() {
                       </ul>
                   </div>
               </div>
-              {/* <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"> */}
               <div className="text-center">
                   <a href="#" className="flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                       <Image src={logo} className="h-6 max-w-max mr-3 sm:h-9" alt="Find Me Fast Logo" width={250} height={250}  />
