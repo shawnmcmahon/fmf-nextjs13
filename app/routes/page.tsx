e5c2f4e2b3b17cf1968ac4cec640969a32d23eb4
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-11-26 10:07:52 -0700

    Import Tailwind template and edit

--- app/routes/page.tsx
+++ app/routes/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import Nav from '../components/nav'
 
 async function getRoutes() {
-  const res = await fetch('http://127.0.0.1:8090/api/collections/routes/records?page=1&perPage=30')
+  const res = await fetch('http://127.0.0.1:8090/api/collections/routes/records?page=1&perPage=30' , 
+   { cache: 'no-store'}
+  );
   const data = await res.json()
   return data?.items as any[]
 }
