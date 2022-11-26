825d2c57f4532a9b1a027149e09b72eeaab6ff2b
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-11-26 14:51:19 -0700

    Add copy to second home section

--- app/page.tsx
+++ app/page.tsx
@@ -211,13 +211,13 @@ export default function page() {
       <section className="bg-white dark:bg-gray-900">
           <div className="items-center max-w-screen-xl px-4 py-8 mx-auto lg:grid lg:grid-cols-4 lg:gap-16 xl:gap-24 lg:py-24 lg:px-6">
               <div className="col-span-2 mb-8">
-                  <p className="text-lg font-medium text-purple-600 dark:text-purple-500">Trusted Worldwide</p>
-                  <h2 className="mt-3 mb-4 text-3xl font-extrabold tracking-tight text-gray-900 md:text-3xl dark:text-white">Trusted by over 600 million users and 10,000 teams</h2>
-                  <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">Our rigorous security and compliance standards are at the heart of all we do. We work tirelessly to protect you and your customers.</p>
+                  <p className="text-lg font-medium text-purple-600 dark:text-purple-500">Unlimited Flexibility</p>
+                  <h2 className="mt-3 mb-4 text-3xl font-extrabold tracking-tight text-gray-900 md:text-3xl dark:text-white">Create routes with unlimited flexibility with Find Me Fast Pro</h2>
+                  <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">Try Find Me Fast risk-free for 30 days. No Credit Card Required.</p>
                   <div className="pt-6 mt-6 space-y-4 border-t border-gray-200 dark:border-gray-700">
                       <div>
                         <a href="#" className="inline-flex items-center text-base font-medium text-purple-600 hover:text-purple-800 dark:text-purple-500 dark:hover:text-purple-700">
-                          Explore Legality Guide
+                          Explore Available Plans
                           <svg className="w-5 h-5 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                         </a>
                       </div>
@@ -243,7 +243,7 @@ export default function page() {
                   <div>
                       <svg className="w-10 h-10 mb-2 text-purple-600 md:w-12 md:h-12 dark:text-purple-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clipRule="evenodd"></path></svg>
                       <h3 className="mb-2 text-2xl font-bold dark:text-white">104+ countries</h3>
-                      <p className="font-light text-gray-500 dark:text-gray-400">Support Google Maps</p>
+                      <p className="font-light text-gray-500 dark:text-gray-400">Support Find Me Fast</p>
                   </div>
                   <div>
                       <svg className="w-10 h-10 mb-2 text-purple-600 md:w-12 md:h-12 dark:text-purple-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path></svg>
