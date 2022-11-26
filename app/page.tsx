3c158e9f7456fef4f26f84631d9707f3014b6e81
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-11-26 13:28:53 -0700

    Replace copy in first section

--- app/page.tsx
+++ app/page.tsx
@@ -142,27 +142,27 @@ export default function page() {
               {/* <!-- Row --> */}
               <div className="items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
                   <div className="text-gray-500 sm:text-lg dark:text-gray-400">
-                      <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">Work with tools you already use</h2>
-                      <p className="mb-8 font-light lg:text-xl">Deliver great service experiences fast - without the complexity of traditional ITSM solutions. Accelerate critical development work, eliminate toil, and deploy changes with ease.</p>
+                      <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">Powered by a tool you love</h2>
+                      <p className="mb-8 font-light lg:text-xl">Deliver great directions fast - with the world&#39;s most trusted GPS. Create routes, add directions, and share maps with ease.</p>
                       {/* <!-- List --> */}
                       <ul role="list" className="pt-8 space-y-5 border-t border-gray-200 my-7 dark:border-gray-700">
                           <li className="flex space-x-3">
                               {/* <!-- Icon --> */}
                               <svg className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
-                              <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Continuous integration and deployment</span>
+                              <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Most Popular Map API</span>
                           </li>
                           <li className="flex space-x-3">
                               {/* <!-- Icon --> */}
                               <svg className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
-                              <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Development workflow</span>
+                              <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Unmatched Reliability</span>
                           </li>
                           <li className="flex space-x-3">
                               {/* <!-- Icon --> */}
                               <svg className="flex-shrink-0 w-5 h-5 text-purple-500 dark:text-purple-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
-                              <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Knowledge management</span>
+                              <span className="text-base font-medium leading-tight text-gray-900 dark:text-white">Exceptional Preformance</span>
                           </li>
                       </ul>
-                      <p className="mb-8 font-light lg:text-xl">Deliver great service experiences fast - without the complexity of traditional ITSM solutions.</p>
+                      <p className="mb-8 font-light lg:text-xl">Deliver great directions fast -  with the world&#39;s most trusted GPS.</p>
                   </div>
                   <div className="flex justify-center">
                     <Image className="hidden max-w-max mb-4 rounded-lg lg:mb-0 lg:flex" src={google} alt="dashboard feature image" width={250} height={250} />
@@ -473,7 +473,7 @@ export default function page() {
           <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-16 lg:px-6">
               <div className="max-w-screen-sm mx-auto text-center">
                   <h2 className="mb-4 text-3xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white">Start your free trial today</h2>
-                  <p className="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg">Try Landwind Platform for 30 days. No credit card required.</p>
+                  <p className="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg">Try Find Me Fast for 30 days. No credit card required.</p>
                   <a href="#" className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">Free trial for 30 days</a>
               </div>
           </div>
