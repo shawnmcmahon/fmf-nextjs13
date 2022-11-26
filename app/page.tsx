926eac88d9ad513e5aae6053746831285003454f
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-11-26 14:55:48 -0700

    Change testimonial section

--- app/page.tsx
+++ app/page.tsx
@@ -261,13 +261,13 @@ export default function page() {
                       <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
                   </svg> 
                   <blockquote>
-                      <p className="text-xl font-medium text-gray-900 md:text-2xl dark:text-white">Landwind is just awesome. It contains tons of predesigned components and pages starting from login screen to complex dashboard. Perfect choice for your next SaaS application.</p>
+                      <p className="text-xl font-medium text-gray-900 md:text-2xl dark:text-white">Find Me Fast is just awesome. At least that is what will be said once there is a real review to post here.</p>
                   </blockquote>
                   <figcaption className="flex items-center justify-center mt-6 space-x-3">
                       {/* <Image className="w-6 h-6 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gouch.png" alt="profile picture" width={250} height={250}  /> */}
                       <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
-                          <div className="pr-3 font-medium text-gray-900 dark:text-white">Micheal Gough</div>
-                          <div className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">CEO at Google</div>
+                          <div className="pr-3 font-medium text-gray-900 dark:text-white">John Doe</div>
+                          <div className="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">Find Me Fast Pro User</div>
                       </div>
                   </figcaption>
               </figure>
